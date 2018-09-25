d3c279fb897f21c7404b81ceaa167aa68c3eee3e
Jawan Higgins <jawanhiggins@jawans-mbp.attlocal.net>
2018-09-25 18:38:16 -0400

    Completed the module

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+        res.render('index', {title: 'Index'})
+    }
+);
+
+app.listen(8080, function () {
+
+    console.log('PS Project Running on port 8080!')
+
+});
